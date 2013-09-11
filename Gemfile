a6ff0374655d36992b67f77fe6b7d78e6fa3ebb1
Thomas Powell <tpowell@appriss.com>
2013-09-11 10:46:28 -0400

    Added kraken by cferdinandi for quick layout.

--- Gemfile
+++ Gemfile
@@ -1,4 +1,7 @@
 source 'https://rubygems.org'
 
 gem 'redcarpet'
+gem 'coderay'
+gem 'kramdown'
+#gem 'github-markup'
 gem 'sinatra'
--- markdown_notes.rb
+++ markdown_notes.rb
@@ -1,13 +1,27 @@
 require 'rubygems'
 require 'redcarpet'
 require 'sinatra'
+require 'coderay'
+require 'kramdown'
+#require 'github/markup'
+
+def render(filename, string)
+  Kramdown::Document.new(string, :template => 'default.erb.html').to_html
+  #GitHub::Markup.render(filename, string)
+end
 
 get '/' do
-  markdown Dir.glob('notes/**/*.md').map { |x| "* [" + x.gsub(/_/,"\\_") + "](/#{x})" }.join("\n")
+  render('index.md', Dir.glob('notes/**/*.md').map { |x| "* [" + x.gsub(/_/,"\\_") + "](/#{x})" }.join("\n"))
 end
 
 get '/*.md' do
-  markdown '[\[Back to File listing\]](/)
+#  render(params[:splat].join('/')+".md",
+#  '[\[Back to File listing\]](/)
+#
+#' + File.read(params[:splat].join('/') + ".md"))
 
-' + File.read(params[:splat].join('.') + ".md")
+  Kramdown::Document.new('[\[Back to File listing\]](/)
+
+' + File.read(params[:splat].join('/') + ".md"), :template => 'default.erb.html').to_html
 end
+
